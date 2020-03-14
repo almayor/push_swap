b6173780caa91727ba22041342eada99cada0436
almayor <mayorovme@gmail.com>
2020-03-14 12:48:27 +0000

    copy_queue added

--- srcs/queue/queue.c
+++ srcs/queue/queue.c
@@ -6,12 +6,13 @@
 /*   By: unite <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/14 10:50:47 by unite             #+#    #+#             */
-/*   Updated: 2020/03/14 11:26:37 by unite            ###   ########.fr       */
+/*   Updated: 2020/03/14 12:47:58 by unite            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "libft.h"
 
 typedef struct	s_node
 {
@@ -26,10 +27,27 @@ typedef struct	s_queue
 	t_node *last;	
 }				t_queue;
 
-int	new_queue(t_queue *queue)
+int free_queue(t_queue *queue)
 {
-	queue->first = 0;
-	queue->last = 0;
+	t_node	*tmp;
+
+	while (queue->first)
+	{
+		tmp = queue->first->next;
+		free(queue->first->data);
+		free(queue->first);
+		queue->first = tmp;
+	}
+	free(queue);
+	return (0);
+}
+
+int	new_queue(t_queue **queue)
+{
+	if (!(*queue = malloc(sizeof(t_queue))))
+		return (1);
+	(*queue)->first = 0;
+	(*queue)->last = 0;
 	return (0);
 }
 
@@ -68,18 +86,63 @@ int	dequeue_queue(t_queue *queue, char **data)
 	return (0);
 }
 
+int	copy_queue(const t_queue *queueA, t_queue **queueB)
+{
+	t_node	*nodeA;
+	char	*dataB;
+
+	if (new_queue(queueB))
+		return (1);
+	nodeA = queueA->first;
+	while (nodeA)
+	{
+		if (!(dataB = ft_strdup(nodeA->data)))
+		{
+			free_queue(*queueB);
+			return (1);
+		}
+		enqueue_queue(*queueB, dataB);
+		nodeA = nodeA->next;
+	}
+	return (0);
+}
+
+// int main () {
+//     t_queue 	*q;
+//     char		*c;
+//     size_t		i;
+
+//     new_queue(&q);
+//     enqueue_queue(q, "Clear drains");
+//     enqueue_queue(q, "Feed cat");
+//     enqueue_queue(q, "Make tea");
+//     enqueue_queue(q, "Solve RC tasks");
+//     enqueue_queue(q, "Tax return");
+//     while (dequeue_queue(q, &c) == 0)
+//     {
+//         printf("%s\n", c);
+//     }
+//     return 0;
+// }
+
 int main () {
-    t_queue 	*q;
+    t_queue 	*q1;
+    t_queue 	*q2;
     char		*c;
     size_t		i;
 
-    new_queue(q);
-    enqueue_queue(q, "Clear drains");
-    enqueue_queue(q, "Feed cat");
-    enqueue_queue(q, "Make tea");
-    enqueue_queue(q, "Solve RC tasks");
-    enqueue_queue(q, "Tax return");
-    while (dequeue_queue(q, &c) == 0)
+    new_queue(&q1);
+    enqueue_queue(q1, "Clear drains");
+    enqueue_queue(q1, "Feed cat");
+    enqueue_queue(q1, "Make tea");
+    enqueue_queue(q1, "Solve RC tasks");
+    enqueue_queue(q1, "Tax return");
+    copy_queue(q1, &q2);
+    while (dequeue_queue(q2, &c) == 0)
+    {
+        printf("%s\n", c);
+    }
+    while (dequeue_queue(q1, &c) == 0)
     {
         printf("%s\n", c);
     }
