9aeb8b781f240f1c8c07d29fb8ba1f150f22454b
almayor <mayorovme@gmail.com>
2020-05-24 05:03:34 +0300

    simple sort when nval > 350

--- src/sorts/advanced_sort/advanced_sort.c
+++ src/sorts/advanced_sort/advanced_sort.c
@@ -6,17 +6,31 @@
 /*   By: unite <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/22 04:31:40 by unite             #+#    #+#             */
-/*   Updated: 2020/05/23 19:54:29 by unite            ###   ########.fr       */
+/*   Updated: 2020/05/24 05:01:44 by unite            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	advanced_sort(t_stack *st_a, t_stack *st_b)
+static void	blind_split_stacks(t_stack *st_a, t_stack *st_b)
+{
+	while (st_a->size > 2)
+	{
+		if (st_a->start->value == 1)
+			perform_operation(st_a, st_b, "ra");
+		else
+			perform_operation(st_a, st_b, "pb");
+	}
+}
+
+void		advanced_sort(t_stack *st_a, t_stack *st_b)
 {
 	if (!issorted_stack(st_a))
 	{
-		advanced_split_stacks(st_a, st_b);
+		if (st_a->size < 350)
+			advanced_split_stacks(st_a, st_b);
+		else
+			blind_split_stacks(st_a, st_b);
 		advanced_merge_stacks(st_a, st_b);
 	}
 }
